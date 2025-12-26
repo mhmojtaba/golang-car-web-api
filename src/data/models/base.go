fa60d3124936018d6c53e0fa2464a0bba69586c1
Mojtaba <mhmojtaba@gmail.com>
2025-12-26 20:53:20 +0330

    [UPDATE] models

--- src/data/models/base.go
+++ src/data/models/base.go
@@ -6,3 +6,9 @@ type City struct {
 	CountryId int
 	Country   Country `gorm:"foreignKey:CountryId"`
 }
+
+type Country struct {
+	BaseModel
+	Name   string `gorm:"size:15;type:string;not null"`
+	Cities *[]City
+}
--- src/data/models/role.go
+++
@@ -1,7 +0,0 @@
-package models
-
-type Role struct {
-	BaseModel
-	Name      string `gorm:"type:string;size:16;not null;unique"`
-	UserRoles *[]UserRole
-}
