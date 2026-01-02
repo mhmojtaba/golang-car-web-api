f5658580f0ba2c4e4cb15c9909db379e60b96a65
Mojtaba <mhmojtaba@gmail.com>
2026-01-02 15:19:19 +0330

    [UPDATE] base model

--- src/data/models/base.go
+++ src/data/models/base.go
@@ -1,5 +1,7 @@
 package models
 
+import "time"
+
 type City struct {
 	BaseModel
 	Name      string `gorm:"size:15;type:string;not null;"`
@@ -9,6 +11,31 @@ type City struct {
 
 type Country struct {
 	BaseModel
-	Name   string `gorm:"size:15;type:string;not null"`
-	Cities *[]City
+	Name    string `gorm:"size:15;type:string;not null"`
+	Cities  []City
+	Company []Company
+}
+
+type CountryYear struct {
+	BaseModel
+	Title         string    `gorm:"size:15;type:string;not null; unique"`
+	Year          int       `gorm:"type:int;uniqueindex;not null"`
+	StartAt       time.Time `gorm:"type:TIMESTAMP with time zone;not null;unique"`
+	EndAt         time.Time `gorm:"type:TIMESTAMP with time zone;not null;unique"`
+	CarModelYears []CarModelYear
+}
+
+type Color struct {
+	BaseModel
+	Name           string `gorm:"size:15;type:string;not null"`
+	HexCode        string `gorm:"size:7;type:string;not null"`
+	CarModelColors []CarModelColor
+}
+
+type File struct {
+	BaseModel
+	Name        string `gorm:"size:100;type:string;not null"`
+	Directory   string `gorm:"size:255;type:string;not null"`
+	Description string `gorm:"size:255;type:string;not null"`
+	MediaType   string `gorm:"size:50;type:string;not null"`
 }
