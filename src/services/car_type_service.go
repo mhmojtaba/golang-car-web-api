b86eed624b959a27b4557df7818c40d2c5b0e574
Mojtaba <mhmojtaba@gmail.com>
2026-01-02 18:53:58 +0330

    [ADD] car car model and car type

---
+++ src/services/car_type_service.go
@@ -0,0 +1,49 @@
+package services
+
+import (
+	"context"
+
+	"github.com/mhmojtaba/golang-car-web-api/api/dto"
+	"github.com/mhmojtaba/golang-car-web-api/config"
+	"github.com/mhmojtaba/golang-car-web-api/data/db"
+	"github.com/mhmojtaba/golang-car-web-api/data/models"
+	"github.com/mhmojtaba/golang-car-web-api/pkg/logging"
+)
+
+type CarTypeService struct {
+	base *BaseService[models.CarType, dto.CreateCarTypeRequest, dto.UpdateCarTypeRequest, dto.CarTypeResponse]
+}
+
+func NewCarTypeService(cfg *config.Config) *CarTypeService {
+	return &CarTypeService{
+		base: &BaseService[models.CarType, dto.CreateCarTypeRequest, dto.UpdateCarTypeRequest, dto.CarTypeResponse]{
+			database: db.GetDb(),
+			logger:   logging.NewLogger(cfg),
+		},
+	}
+}
+
+// Create
+func (s *CarTypeService) Create(ctx context.Context, req *dto.CreateCarTypeRequest) (*dto.CarTypeResponse, error) {
+	return s.base.Create(ctx, req)
+}
+
+// Update
+func (s *CarTypeService) Update(ctx context.Context, id int, req *dto.UpdateCarTypeRequest) (*dto.CarTypeResponse, error) {
+	return s.base.Update(ctx, id, req)
+}
+
+// Delete
+func (s *CarTypeService) Delete(ctx context.Context, id int) error {
+	return s.base.Delete(ctx, id)
+}
+
+// Get By Id
+func (s *CarTypeService) GetById(ctx context.Context, id int) (*dto.CarTypeResponse, error) {
+	return s.base.GetByID(ctx, id)
+}
+
+// Get By Filter
+func (s *CarTypeService) GetByFilter(ctx context.Context, req *dto.PaginationResultWithFilter) (*dto.Pagination[dto.CarTypeResponse], error) {
+	return s.base.GetByFilter(ctx, req)
+}
