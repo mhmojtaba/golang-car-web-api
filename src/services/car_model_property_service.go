329b6c7fa90a099af46603df13374b3bc26e5bc4
Mojtaba <mhmojtaba@gmail.com>
2026-01-03 17:38:08 +0330

    [ADD] car model property

---
+++ src/services/car_model_property_service.go
@@ -0,0 +1,52 @@
+package services
+
+import (
+	"context"
+
+	"github.com/mhmojtaba/golang-car-web-api/api/dto"
+	"github.com/mhmojtaba/golang-car-web-api/config"
+	"github.com/mhmojtaba/golang-car-web-api/data/db"
+	"github.com/mhmojtaba/golang-car-web-api/data/models"
+	"github.com/mhmojtaba/golang-car-web-api/pkg/logging"
+)
+
+type CarModelPropertyService struct {
+	base *BaseService[models.CarModelProperty, dto.CreateCarModelPropertyRequest, dto.UpdateCarModelPropertyRequest, dto.CarModelPropertyResponse]
+}
+
+func NewCarModelPropertyService(cfg *config.Config) *CarModelPropertyService {
+	return &CarModelPropertyService{
+		base: &BaseService[models.CarModelProperty, dto.CreateCarModelPropertyRequest, dto.UpdateCarModelPropertyRequest, dto.CarModelPropertyResponse]{
+			database: db.GetDb(),
+			logger:   logging.NewLogger(cfg),
+			Preloads: []preload{
+				{string: "Property.Category"},
+			},
+		},
+	}
+}
+
+// Create
+func (s *CarModelPropertyService) Create(ctx context.Context, req *dto.CreateCarModelPropertyRequest) (*dto.CarModelPropertyResponse, error) {
+	return s.base.Create(ctx, req)
+}
+
+// Update
+func (s *CarModelPropertyService) Update(ctx context.Context, id int, req *dto.UpdateCarModelPropertyRequest) (*dto.CarModelPropertyResponse, error) {
+	return s.base.Update(ctx, id, req)
+}
+
+// Delete
+func (s *CarModelPropertyService) Delete(ctx context.Context, id int) error {
+	return s.base.Delete(ctx, id)
+}
+
+// Get By Id
+func (s *CarModelPropertyService) GetById(ctx context.Context, id int) (*dto.CarModelPropertyResponse, error) {
+	return s.base.GetByID(ctx, id)
+}
+
+// Get By Filter
+func (s *CarModelPropertyService) GetByFilter(ctx context.Context, req *dto.PaginationResultWithFilter) (*dto.Pagination[dto.CarModelPropertyResponse], error) {
+	return s.base.GetByFilter(ctx, req)
+}
